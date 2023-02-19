5b07fa21c15b55d45518071afa19c307aef896f3
Mark Tucker <markdaddy@Marks-MacBook-Air.local>
2023-02-19 15:22:54 -0700

    added server and basic router, created first get endpoint

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,25 @@
 // implement your posts router here
+const express = require('express');
+const Post = require('./posts-model');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.status(200).json(posts);
+    }
+    catch(err) {
+        res.status(500).json({ message: "The posts information could not be retrieved" })
+    }
+  });
+  
+  router.get('/:id', (req, res) => {
+    res.status(200).send('hello from the GET /users/:id endpoint');
+  });
+  
+  router.post('/', (req, res) => {
+    res.status(200).send('hello from the POST /users endpoint');
+  });
+
+module.exports = router;
