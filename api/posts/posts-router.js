1c2dec36fe549499780b55a46e074d7686c3874e
Mark Tucker <markdaddy@Marks-MacBook-Air.local>
2023-02-19 17:27:11 -0700

    completed comments endpoint. all tests pass

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -84,5 +84,23 @@ router.delete('/:id', async (req, res) => {
     }
 })
 
+router.get('/:id/comments', (req, res) => {
+    Post.findById(req.params.id)
+        .then(stuff => {
+            if(!stuff) {
+                res.status(404).json({ message: "The post with the specified ID does not exist" })
+            }
+            else {
+                return Post.findPostComments(req.params.id)
+            }
+        })
+        .then(comments => {
+            res.status(200).json(comments)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "The comments information could not be retrieved" })
+        })
+})
+
 
 module.exports = router;
